2870923bc382de36f40f2eec48d80cac4c12c465
Th0rS1r <36897421+Th0rS1r@users.noreply.github.com>
2019-05-03 09:15:39 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,10 +5,10 @@ module.exports = client => { // eslint-disable-line no-unused-vars
 
   console.log(chalk.bgGreen.black(`${client.user.username} is online on ${client.guilds.size} servers!`));
   console.log(chalk.bgGreen.black('The connection was successful. Press CTRL+C to exit.'));
-  client.setInterval(() => {
+  /*client.setInterval(() => {
 		const activity = activities[Math.floor(Math.random() * activities.length)];
 		client.user.setActivity(activity.text, { type: activity.type });
-	}, 3600000);
+	}, 3600000);*/
   //client.user.setStatus("idle");
   //client.user.setActivity(`${process.env.prefix}help`, true);
  
